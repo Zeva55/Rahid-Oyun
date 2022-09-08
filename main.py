c760977c0c8b23e6790a420026db798ac8e1c9cb
Zeva55 <113102760+Zeva55@users.noreply.github.com>
2022-09-08 19:45:00 +0400

    Update main.py

--- main.py
+++ main.py
@@ -66,10 +66,10 @@ def button(update, context):
 def command_start(update, context: CallbackContext):
     if update.effective_chat.type == "private":
         
-        addme = InlineKeyboardButton(text="🧚 Qrupa əlavə edin!", url="https://t.me/Rahid_Crocodile_Bot?startgroup=a")
-        sohbet = InlineKeyboardButton(text="💬 Oyun Qrupu", url="https://t.me/Dark_Region")
-        oyun = InlineKeyboardButton(text="🤖 Botlar", url="https://t.me/Rahid_44")
-        admin = InlineKeyboardButton(text="👨🏻‍💻 Sahib", url="https://t.me/Rahid_2003")
+        addme = InlineKeyboardButton(text="🧚 Qrupa əlavə edin!", url="https://t.me/ZevaSozizahBot?startgroup=a")
+        sohbet = InlineKeyboardButton(text="💬 Oyun Qrupu", url="https://t.me/ZevaSUP")
+        oyun = InlineKeyboardButton(text="🤖 Botlar", url="https://t.me/ZevaBots")
+        admin = InlineKeyboardButton(text="👨🏻‍💻 Sahib", url="https://t.me/Zevadida")
 
         keyboard = [[addme],[sohbet],[oyun],[admin]]
         reply_markup = InlineKeyboardMarkup(keyboard)
